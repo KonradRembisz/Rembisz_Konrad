1306e779b68d5e92225d1d5dfae6e3fbb478c411
KonradRembisz <quake1528@gmail.com>
2023-02-14 10:37:59 +0100

    documentation

--- sequence.h
+++ sequence.h
@@ -2,23 +2,46 @@
 #define __SEQUENCE_H__
 
 #include <iostream>
-
-
+    //! \brief Klasa Sequence
+    //! Klasa Sequence jest definicją obiektu, który przechowuje dwie wartości typu int i udostępnia metody do ich manipulacji i wykonywania na nich prostych działań matematycznych
 class Sequence{
 public:
-    
+    //! \brief Konstruktor domyślny
     Sequence(int x, int y);
-    
+    //! \brief Konstruktor kopiujący
     Sequence(Sequence kopia);
-    
+    //! \brief Destruktor
     ~Sequence();
-    
+    //! \brief Funkcje dostępu do składowych
     int accessX();
     
     int accessY();
-    
+    //! \brief Wzór na rozkład F-Snedecora
+    /**
+    * \f[
+    *   F = \frac{\left(\frac{S_1}{n_1}\right)}{\left(\frac{S_2}{n_2}\right)}
+    * \f]
+    * 
+    * gdzie \f$S_1\f$ i \f$S_2\f$ są odchyleniami standardowymi dla dwóch badanych próbek, 
+    * a \f$n_1\f$ i \f$n_2\f$ są ich odpowiednimi rozmiarami.
+    * 
+    * Rozkład F-Snedecora jest używany do testowania hipotez dotyczących równowagi 
+    * średnich dla dwóch lub więcej grupy.
+    */
     float dodaj();
-
+    //! \brief Wartości krytyczne dla rozkładu F-Snedecora
+    //! | Stopień swobody 1 | Stopień swobody 2 | Wartość krytyczna |
+    //! |---|---|----|
+    //! | 1 | 1 |  12.706 |
+    //! | 2 | 2 |  4.303 |
+    //! | 3 | 3 |  3.182 |
+    //! | 4 | 4 |  2.776 |
+    //! | 5 | 5 |  2.571 |
+    //! | 6 | 6 |  2.447 |
+    //! | 7 | 7 |  2.365 |
+    //! | 8 | 8 |  2.306 |
+    //! | 9 | 9 |  2.262 |
+    //! | 10 | 10 |  2.228 |
     float odejmij();
 private:
     int x, y;
--- statistics.h
+++ statistics.h
@@ -3,23 +3,57 @@
 
 #include <iostream>
 
-
-
+    //! \brief Klasa Statistics
+    //! Klasa ta zawiera funkcje do obliczania statystyk matematycznych.
 class Statistics{
 public:
-    
+    //! \brief Konstruktor domyślny
+    //! Tworzy obiekt klasy Statistics z wartościami x i y jako argumentami.
+    //! @param x Pierwsza wartość
+    //! @param y Druga wartość
     Statistics(int x, int y);
-    
+    //! \brief Konstruktor kopiujący
+    //! Tworzy kopię obiektu klasy Statistics z innego obiektu klasy Statistics jako argumentu.
+    //! @param kopia Obiekt klasy Statistics, który ma być skopiowany
     Statistics(Statistics kopia);
-    
+    //! \brief Destruktor
+    //! Niszczy obiekt klasy Statistics.
     ~Statistics();
-    
+    //! \brief Dostęp do wartości x
+    //! Zwraca wartość x z obiektu klasy Statistics.
+    //! @return Wartość x
     int accessX();
-    
+    //! \brief Dostęp do wartości y
+    //! Zwraca wartość y z obiektu klasy Statistics.
+    //! @return Wartość y
     int accessY();
-    
+    //! \brief Całka Poissona
+    /**
+    * \f$ \int_{a}^{b} \frac{e^{-x} x^n}{n!} \f$
+    * 
+    * Jest to wzór na całkę Poissona.
+    * gdzie \f$ x \f$ jest zmienną niezależną, a \f$ n \f$ jest niezależną zmienną całkową.
+    * \f$ a \f$ i \f$ b \f$ oznaczają granice całkowania.
+    */
     float odejmij();
-    
+    /**
+    * @brief Rozmycie Gaussa.
+    * 
+    * @details Rozmycie Gaussa jest jednym z popularnych filtrów obrazów, którego celem jest usuwanie szumów. 
+    * Wzór na rozmycie Gaussa jest następujący:
+    * 
+    * @f$ G(x, y) = \frac{1}{2 \pi \sigma^2} e^{-(x^2 + y^2) / 2 \sigma^2} @f$
+    * 
+    *  gdzie @f$ x @f$ i @f$ y @f$ oznaczają współrzędne punktu w obrazie, a @f$ \sigma @f$ jest stałą określającą stopień rozmycia.
+    * 
+    * @param x Współrzędna @f$ x @f$ punktu w obrazie.
+    * @param y Współrzędna @f$ y @f$ punktu w obrazie.
+    * @param sigma Stała określająca stopień rozmycia.
+    * 
+    * @return Wartość rozmycia Gaussa dla danego punktu w obrazie.
+    */
+    //! \brief Efekt małego i dużego rozmycia gaussowskiego
+    //! \image html gaus.png
     float dodaj();
     
 
